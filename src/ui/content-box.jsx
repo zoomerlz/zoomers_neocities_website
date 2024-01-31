14a5ac198145189528337d27a9949816910773b9
zoomerlz <roskosch.robin@web.de>
2024-01-31 21:06:47 +0100

    implemented code chunks and lazy load

--- src/ui/content-box.jsx
+++ src/ui/content-box.jsx
@@ -1,20 +1,22 @@
+import { useState, Suspense, lazy } from 'react';
 import { Routes, Route } from "react-router-dom";
 
-import Home from "../pages/home";
-import Projects from "../pages/projects";
-import Interests from "../pages/interests";
-import Media from "../pages/media";
-import Settings from "../pages/settings";
+const Home = lazy(() => import("../pages/home"));
+const Projects = lazy(() => import("../pages/projects"));
+const Interests = lazy(() => import("../pages/interests"));
+const Media = lazy(() => import("../pages/media"));
+const Settings = lazy(() => import("../pages/settings"));
 
 export default function ContentBox() {
   return (
     <div className="w-full h-full backdrop-blur border-2 border-double border-primary bg-primaryDark/60 p-5 text-2xl">
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/interests" element={<Interests />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/media" element={<Media />} />
-        <Route path="/settings" element={<Settings />} />
+      {/*<Route path="/" element={<Home />} /> */}
+        <Route path="/" element={<Suspense fallback={<>Loading...</>}><Home /></Suspense>}/>
+        <Route path="/interests" element={<Suspense fallback={<>Loading...</>}><Interests /></Suspense>}/>
+        <Route path="/projects" element={<Suspense fallback={<>Loading...</>}><Projects /></Suspense>}/>
+        <Route path="/media" element={<Suspense fallback={<>Loading...</>}><Media /></Suspense>}/>
+        <Route path="/settings" element={<Suspense fallback={<>Loading...</>}><Settings /></Suspense>}/>
       </Routes>
     </div>
   );
